653ff5214c5d65744ffd801f7bab04c3ee5017f7
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-09-19 17:50:49 -0500

    Create Discord URI

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
-const app = require('express')();
-
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+app.use(express.json());
 
 app.listen(
     PORT,
     () => console.log(`alive on http://localhost:${PORT}`)
 );
+
+//Discord data request handler (returns)
+app.get('/discord', (req, res) => {
+    res.status(200).send({
+        name: "",
+        roles: []
+    })
+});
