a373561745b9c6ed79be8ae338a0f9865f35b35b
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-10-02 17:26:24 -0500

    Login With Playfab

---
+++ src/API/public/scripts/index.js
@@ -0,0 +1,24 @@
+function DoExampleLoginWithCustomID(){
+    PlayFab.settings.titleId = document.getElementById("titleId").value;
+    var loginRequest = {
+        // Currently, you need to look up the required and optional keys for this object in the API reference for LoginWithCustomID. See the Request Headers and Request Body.
+        TitleId: PlayFab.settings.titleId,
+        CustomId: document.getElementById("customId").value,
+        CreateAccount: true
+    };
+
+    PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
+}
+
+// callback functions take two parameters: result and error
+// see callback functions in JavaScript if unclear
+var LoginCallback = function (result, error) {
+    if (result !== null) {
+        document.getElementById("resultOutput").innerHTML = "Success!";
+    } else if (error !== null) {
+        document.getElementById("resultOutput").innerHTML =
+            "Something went wrong with your API call.\n" +
+            "Here's some debug information:\n" +
+            PlayFab.GenerateErrorReport(error);
+    }
+}
