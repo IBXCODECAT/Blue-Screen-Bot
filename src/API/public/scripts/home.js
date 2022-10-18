1ae6e7b9656cf5bca5c615cf1badc27f0c0c2305
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-10-18 10:04:30 -0500

    All playfab requests made on same URI

--- src/API/public/scripts/home.js
+++ src/API/public/scripts/home.js
@@ -36,10 +36,45 @@ var LoginCallback = function (result, error) {
 
         console.log("PlayFab ID: " + sessionStorage.getItem('PlayFabID'));
 
-        
-        //window.location.href = "/auth/discord"; //Redirect to discord authorization prompt
-
     } else if (error !== null) {
         document.getElementById("error_text").innerText = error.errorMessage;
+    }
+}
+
+const playfabID = sessionStorage.getItem("PlayFabID");
+const discord_id = "TEST_ID4";
+
+console.log("Player ID: " + playfabID);
+console.log("Discord ID: " + discord_id);
+
+
+function LinkAccountID()
+{
+    var linkRequest = {
+        
+        // Currently, you need to look up the required and optional keys for this object in the API reference for LoginWithCustomID. See the Request Headers and Request Body.
+        CustomId: discord_id,
+        ForceLink: true
+    };
+
+    try
+    {
+        
+        PlayFabClientSDK.LinkCustomID(linkRequest, LinkingCallback);
+    }
+    catch (res)
+    {
+        console.error("There was a problem calling this method.. " + res)
+    }
+}
+
+var LinkingCallback = function (result, error) {
+    if(result !== null) {
+        console.log("Linked?");
+        console.log(result);
+    } else if (error !== null) {
+        var error = PlayFab.GenerateErrorReport(error);
+        console.error(error);
+        console.log("I Ran!");
     }
 }
