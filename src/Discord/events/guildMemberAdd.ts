fb95c4d03be5a422b4fece7f5369bdac390e43c9
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-11-08 14:46:34 -0600

    Update guildMemberAdd.ts

--- src/Discord/events/guildMemberAdd.ts
+++ src/Discord/events/guildMemberAdd.ts
@@ -12,7 +12,9 @@ export = {
     {
         if(member.guild.id == guild)
         {
-            member.send(`Hello <@${member.user?.id}>! We are so glad you are have joined our gaming community!
+            try
+            {
+                member.send(`Hello <@${member.user?.id}>! We are so glad you are have joined our gaming community!
 
 Here are a few important channels that you should check out!
 
@@ -23,6 +25,12 @@ Here are a few important channels that you should check out!
 The Blue Screen Studios server is owned and operated by the game developers at Blue Screen Studios. If you ever have any questions about anything, please feel free to ask! We hope you enjoy it here!
             
 *I am a bot, and this message was automated...*`);
+            }
+            catch
+            {
+                console.log("Unable to DM User");
+            }
+
             
             const channel = await client.channels.fetch(log);
             (channel as TextChannel).send(`**Member Join:** <@${member.user.id}> [${member.user.id}]`);
