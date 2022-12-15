3592a9e0707515ff4f45ded466907929fcd2a440
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-12-15 17:22:53 -0600

    Update for deployment

--- src/Discord/events/ready.ts
+++ src/Discord/events/ready.ts
@@ -12,7 +12,7 @@ export = {
         console.log(`Discord Client [${client.application?.id}] is ready!`);
         
         //RegisterSlashCommands(client); //Call this functiomn to register all slash commands
-        //DeleteCommands(); //Call this function to delete all global commands
+        DeleteCommands(); //Call this function to delete all global commands
 
         client.user?.setActivity(`${client.guilds.cache.size} servers!`, ({type: ActivityType.Watching }))
     }
