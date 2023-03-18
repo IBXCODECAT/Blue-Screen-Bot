162026d1f22af56054fa6f5fee338fd8ffb2e7b5
IBXCODECAT <nathan.schmitt7@gmail.com>
2023-03-18 15:47:54 -0500

    Fix TS-Checking

--- pages/api/interactions/index.ts
+++ pages/api/interactions/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { APIApplicationCommandInteraction, APIChatInputApplicationCommandInteraction, APIInteractionResponse, MessageFlags } from "discord-api-types/v10"
 import withDiscordInteraction from "middlewares/discord-interaction"
 import withErrorHandler from "middlewares/error-handler"
-import { getGlobalCommands } from "services/discord"
 
 const BASE_RESPONSE = { type: 4, flags: [ MessageFlags.Ephemeral ] }
 
@@ -10,15 +9,13 @@ const INVALID_COMMAND_RESPONSE = { ...BASE_RESPONSE, data: { content: "You have 
 const PING_COMMAND_RESPONSE = { ...BASE_RESPONSE, data: { content: "Pong! As of March 9 2023 I started using an interactions endpoint URI and therefore I am unable to calculate ping times :(" } }
 const BOT_SUPPORT_RESPONSE = { ...BASE_RESPONSE, data: { content: "Here is the link to the bot support server!\n\nhttps://discord.gg/Zy5uXQUZXx" }}
 
+//const HELP_RESPONSE = { ...BASE_RESPONSE, data: { content: "Need Help? No problem! You can find a list of my commands here:\n\nhttps://blue-bot.vercel"}};
 // disable body parsing, need the raw body as per https://discord.com/developers/docs/interactions/slash-commands#security-and-authorization
 export const config = {
   api: {
     bodyParser: false,
   },
 }
-
-const { data } = await getGlobalCommands()
-console.log(data);
 
 //THIS IS THE COMMAND HANDLER I THINK
 const handler = async (
@@ -31,6 +28,7 @@ const handler = async (
 
   switch (name) {
     case "help" || "Need Help?":
+      //return res.status(200).json()
       return;
     case "ping":
       return res.status(200).json(PING_COMMAND_RESPONSE)
--- pages/api/oauth2/index.ts
+++ pages/api/oauth2/index.ts
@@ -1,4 +1,6 @@
 // req = HTTP incoming message, res = HTTP server response
+
+//@ts-expect-error
 export default function handler(req: any, res: any) {
     return res.status(404).json({"code": 404, "type": "Not Found"});
 }
